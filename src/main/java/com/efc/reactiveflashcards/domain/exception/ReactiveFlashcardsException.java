1c893c77dea54857d112a4b08a8e206d8c1a2d1d
eduardocaas <eduardofc.dev@gmail.com>
2023-06-13 23:11:42 -0300

    API Exception

---
+++ src/main/java/com/efc/reactiveflashcards/domain/exception/ReactiveFlashcardsException.java
@@ -0,0 +1,8 @@
+package com.efc.reactiveflashcards.domain.exception;
+
+public class ReactiveFlashcardsException extends RuntimeException {
+
+    public ReactiveFlashcardsException(final String message) {
+        super(message);
+    }
+}
