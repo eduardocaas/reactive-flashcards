b95dbc138076c3d8f81b104684992c2d5401d92f
eduardocaas <eduardofc.dev@gmail.com>
2023-06-14 22:55:42 -0300

    New exceptions handler

--- src/main/java/com/efc/reactiveflashcards/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/efc/reactiveflashcards/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.efc.reactiveflashcards.api.exceptionhandler;
 
 import com.efc.reactiveflashcards.api.controller.response.ProblemResponse;
+import com.efc.reactiveflashcards.domain.exception.NotFoundException;
 import com.efc.reactiveflashcards.domain.exception.ReactiveFlashcardsException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,15 +11,17 @@ import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebExceptionHandler;
 import reactor.core.publisher.Mono;
 
-import static com.efc.reactiveflashcards.domain.exception.BaseErrorMessage.GENERIC_EXCEPTION;
-import static com.efc.reactiveflashcards.domain.exception.BaseErrorMessage.GENERIC_METHOD_NOT_ALLOW;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
+import javax.validation.ConstraintViolationException;
+
+import static com.efc.reactiveflashcards.domain.exception.BaseErrorMessage.*;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Component
@@ -33,6 +36,10 @@ public class ApiExceptionHandler implements WebExceptionHandler {
     public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
         return Mono.error(ex)
                 .onErrorResume(MethodNotAllowedException.class, e -> handleMethodNotAllowedException(exchange, e))
+                .onErrorResume(NotFoundException.class, e -> handleNotFoundException(exchange, e))
+                .onErrorResume(ResponseStatusException.class, e -> handleResponseStatusException(exchange, e))
+                //.onErrorResume(ConstraintViolationException.class)
+                //.onErrorResume(WebExchangeBindException.class)
                 .onErrorResume(ReactiveFlashcardsException.class, e -> handleReactiveFlashcardsException(exchange, e))
                 .onErrorResume(Exception.class, e -> handleException(exchange, e))
                 .onErrorResume(JsonProcessingException.class, e -> handleJsonProcessingException(exchange, e))
@@ -49,6 +56,28 @@ public class ApiExceptionHandler implements WebExceptionHandler {
                 .doFirst(() -> log.error("==== MethodNotAllowedException: Method [{}] is not allowed at [{}]",
                                         exchange.getRequest().getMethod(),
                                         exchange.getRequest().getPath().value(), ex))
+                .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
+    }
+
+    private Mono<Void> handleNotFoundException(final ServerWebExchange exchange,
+                                                       final NotFoundException ex) {
+        return Mono.fromCallable(() -> {
+                    prepareExchange(exchange, NOT_FOUND);
+                    return ex.getMessage();
+                })
+                .map(message -> buildError(NOT_FOUND, message))
+                .doFirst(() -> log.error("==== NotFoundException", ex))
+                .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
+    }
+
+    private Mono<Void> handleResponseStatusException(final ServerWebExchange exchange,
+                                                       final ResponseStatusException ex) {
+        return Mono.fromCallable(() -> {
+                    prepareExchange(exchange, NOT_FOUND);
+                    return GENERIC_NOT_FOUND.getMessage();
+                })
+                .map(message -> buildError(NOT_FOUND, message))
+                .doFirst(() -> log.error("==== ResponseStatusException", ex))
                 .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
     }
 
@@ -82,7 +111,7 @@ public class ApiExceptionHandler implements WebExceptionHandler {
                 })
                 .map(message -> buildError(METHOD_NOT_ALLOWED, message))
                 .doFirst(() -> log.error("==== JsonProcessingException: Failed to map exception for the request {} ",
-                                        exchange.getRequest().getMethod(), ex))
+                                        exchange.getRequest().getPath().value(), ex))
                 .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
     }
 
