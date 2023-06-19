d509cd079c1d1b5a9dea2ff9a04e36a20fd6969b
eduardocaas <eduardofc.dev@gmail.com>
2023-06-18 22:55:35 -0300

    Mapping with id and request

--- src/main/java/com/efc/reactiveflashcards/api/mapper/UserMapper.java
+++ src/main/java/com/efc/reactiveflashcards/api/mapper/UserMapper.java
@@ -15,6 +15,11 @@ public interface UserMapper {
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     UserDocument toDocument(final UserRequest request);
+
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    UserDocument toDocument(final UserRequest request, final String id);
+
     UserResponse toResponse(final UserDocument document);
 
 }
