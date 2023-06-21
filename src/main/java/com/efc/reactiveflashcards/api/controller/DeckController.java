fc63e260d0659068fa889552c7301af38501e103
eduardocaas <eduardofc.dev@gmail.com>
2023-06-20 23:19:22 -0300

    Deck controller with save method

---
+++ src/main/java/com/efc/reactiveflashcards/api/controller/DeckController.java
@@ -0,0 +1,36 @@
+package com.efc.reactiveflashcards.api.controller;
+
+import com.efc.reactiveflashcards.api.controller.request.DeckRequest;
+import com.efc.reactiveflashcards.api.controller.response.DeckResponse;
+import com.efc.reactiveflashcards.api.mapper.DeckMapper;
+import com.efc.reactiveflashcards.domain.service.DeckService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Validated
+@RestController
+@RequestMapping("decks")
+@Slf4j
+@AllArgsConstructor
+public class DeckController {
+
+    private final DeckService deckService;
+    private final DeckMapper deckMapper;
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(CREATED)
+    public Mono<DeckResponse> save(@Valid @RequestBody final DeckRequest request) {
+        return deckService
+                .save(deckMapper.toDocument(request))
+                .doFirst(() -> log.info("==== saving a deck with follow data {}", request))
+                .map(deckMapper::toResponse);
+    }
+}
