e94381937a711099561b131c91714e91cd6efbb5
eduardocaas <eduardofc.dev@gmail.com>
2023-06-21 23:47:03 -0300

    FindById implemented

--- src/main/java/com/efc/reactiveflashcards/api/controller/DeckController.java
+++ src/main/java/com/efc/reactiveflashcards/api/controller/DeckController.java
@@ -3,7 +3,9 @@ package com.efc.reactiveflashcards.api.controller;
 import com.efc.reactiveflashcards.api.controller.request.DeckRequest;
 import com.efc.reactiveflashcards.api.controller.response.DeckResponse;
 import com.efc.reactiveflashcards.api.mapper.DeckMapper;
+import com.efc.reactiveflashcards.core.validation.MongoId;
 import com.efc.reactiveflashcards.domain.service.DeckService;
+import com.efc.reactiveflashcards.domain.service.query.DeckQueryService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +25,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class DeckController {
 
     private final DeckService deckService;
+    private final DeckQueryService deckQueryService;
     private final DeckMapper deckMapper;
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
@@ -31,6 +34,13 @@ public class DeckController {
         return deckService
                 .save(deckMapper.toDocument(request))
                 .doFirst(() -> log.info("==== saving a deck with follow data {}", request))
+                .map(deckMapper::toResponse);
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE, value = "{id}")
+    public Mono<DeckResponse> findById(@PathVariable @Valid @MongoId(message = "{deckController.id}") final String id) {
+        return deckQueryService.findById(id)
+                .doFirst(() -> log.info("==== finding deck with id: {}", id))
                 .map(deckMapper::toResponse);
     }
 }
