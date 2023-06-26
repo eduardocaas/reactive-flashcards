e096148f6e46dfb29d659113cd27bcd6721e1b9d
eduardocaas <eduardofc.dev@gmail.com>
2023-06-25 23:57:40 -0300

    findByEmail

--- src/main/java/com/efc/reactiveflashcards/domain/service/query/UserQueryService.java
+++ src/main/java/com/efc/reactiveflashcards/domain/service/query/UserQueryService.java
@@ -24,6 +24,14 @@ public class UserQueryService {
                 .doFirst(() -> log.info("==== try to find user with id: {}", id))
                 .filter(Objects::nonNull)
                 .switchIfEmpty(Mono.defer(() ->
-                        Mono.error(new NotFoundException(USER_NOT_FOUND.params(id).getMessage()))));
+                        Mono.error(new NotFoundException(USER_NOT_FOUND.params("id", id).getMessage()))));
+    }
+
+    public Mono<UserDocument> findByEmail(final String email) {
+        return userRepository.findByEmail(email)
+                .doFirst(() -> log.info("==== try to find user with email: {}", email))
+                .filter(Objects::nonNull)
+                .switchIfEmpty(Mono.defer(() ->
+                        Mono.error(new NotFoundException(USER_NOT_FOUND.params("email", email).getMessage()))));
     }
 }
