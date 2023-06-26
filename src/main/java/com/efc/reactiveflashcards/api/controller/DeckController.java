29481d6675c09924bbc86ded7a8d6b6808484678
eduardocaas <eduardofc.dev@gmail.com>
2023-06-25 23:59:41 -0300

    findAll method

--- src/main/java/com/efc/reactiveflashcards/api/controller/DeckController.java
+++ src/main/java/com/efc/reactiveflashcards/api/controller/DeckController.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
@@ -28,12 +29,10 @@ public class DeckController {
     private final DeckQueryService deckQueryService;
     private final DeckMapper deckMapper;
 
-    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    @ResponseStatus(CREATED)
-    public Mono<DeckResponse> save(@Valid @RequestBody final DeckRequest request) {
-        return deckService
-                .save(deckMapper.toDocument(request))
-                .doFirst(() -> log.info("==== saving a deck with follow data {}", request))
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public Flux<DeckResponse> findAll() {
+        return deckQueryService.findAll()
+                .doFirst(() -> log.info("==== finding all decks"))
                 .map(deckMapper::toResponse);
     }
 
@@ -43,4 +42,15 @@ public class DeckController {
                 .doFirst(() -> log.info("==== finding deck with id: {}", id))
                 .map(deckMapper::toResponse);
     }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @ResponseStatus(CREATED)
+    public Mono<DeckResponse> save(@Valid @RequestBody final DeckRequest request) {
+        return deckService
+                .save(deckMapper.toDocument(request))
+                .doFirst(() -> log.info("==== saving a deck with follow data {}", request))
+                .map(deckMapper::toResponse);
+    }
+
+
 }
