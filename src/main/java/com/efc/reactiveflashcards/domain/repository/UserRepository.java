582dd83e3e19359e5bf134555fe7980d00fd1320
eduardocaas <eduardofc.dev@gmail.com>
2023-06-25 23:57:31 -0300

    FindByEmail

--- src/main/java/com/efc/reactiveflashcards/domain/repository/UserRepository.java
+++ src/main/java/com/efc/reactiveflashcards/domain/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.efc.reactiveflashcards.domain.repository;
 import com.efc.reactiveflashcards.domain.document.UserDocument;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends ReactiveMongoRepository<UserDocument, String> {
+
+    Mono<UserDocument> findByEmail(final String email);
 }
