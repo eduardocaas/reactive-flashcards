dededb605cbb5ef896e4c5acb982d1bbd9d063b6
eduardocaas <eduardofc.dev@gmail.com>
2023-06-25 23:57:54 -0300

    Refactor

--- src/main/java/com/efc/reactiveflashcards/domain/document/Question.java
+++ src/main/java/com/efc/reactiveflashcards/domain/document/Question.java
@@ -1,6 +1,10 @@
 package com.efc.reactiveflashcards.domain.document;
 
+import java.time.OffsetDateTime;
+
 public record Question(String asked,
+                       OffsetDateTime askedIn,
                        String answered,
+                       OffsetDateTime answeredIn,
                        String expected) {
 }
