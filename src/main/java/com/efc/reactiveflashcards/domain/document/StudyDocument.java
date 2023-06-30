50750b327463e710c3744f03409be192dcdce18b
eduardocaas <eduardofc.dev@gmail.com>
2023-06-29 23:34:19 -0300

    Builder and fields refactor

--- src/main/java/com/efc/reactiveflashcards/domain/document/StudyDocument.java
+++ src/main/java/com/efc/reactiveflashcards/domain/document/StudyDocument.java
@@ -1,6 +1,8 @@
 package com.efc.reactiveflashcards.domain.document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -8,13 +10,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "studies")
 public record StudyDocument(@Id
                         String id,
+                        @Field("user_id")
                         String userId,
+                        @Field("study_deck")
                         StudyDeck studyDeck,
                         List<Question> questions,
                         @CreatedDate
@@ -24,14 +29,66 @@ public record StudyDocument(@Id
                         @Field("updated_at")
                         OffsetDateTime updatedAt) {
 
-    @Builder(toBuilder = true)
-    public StudyDocument { }
-
-    public void addQuestion(final Question question) {
-        questions.add(question);
+    public static StudyDocumentBuilder builder() {
+        return new StudyDocumentBuilder();
     }
+
+    public StudyDocumentBuilder toBuilder() {
+        return new StudyDocumentBuilder(id, userId, studyDeck, questions, createdAt, updatedAt);
+    }
+
     public Question getLastQuestionPending() {
         return questions.stream().filter(question ->
                 Objects.isNull(question.answeredIn())).findFirst().orElseThrow();
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class StudyDocumentBuilder {
+        private String id;
+        private String userId;
+        private StudyDeck studyDeck;
+        private List<Question> questions = new ArrayList<>();
+        private OffsetDateTime createdAt;
+        private OffsetDateTime updatedAt;
+
+        public StudyDocumentBuilder id(final String id) {
+            this.id = id;
+            return this;
+        }
+
+        public StudyDocumentBuilder userId(final String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public StudyDocumentBuilder studyDeck(final StudyDeck studyDeck) {
+            this.studyDeck = studyDeck;
+            return this;
+        }
+
+        public StudyDocumentBuilder questions(final List<Question> questions) {
+            this.questions = questions;
+            return this;
+        }
+
+        public StudyDocumentBuilder question(final Question question) {
+            this.questions.add(question);
+            return this;
+        }
+
+        public StudyDocumentBuilder createdAt(final OffsetDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public StudyDocumentBuilder updatedAt(final OffsetDateTime updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public StudyDocument build() {
+            return new StudyDocument(id, userId, studyDeck, questions, createdAt, updatedAt);
+        }
     }
 }
