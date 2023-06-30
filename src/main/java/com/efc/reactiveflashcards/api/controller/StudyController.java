8eaf636976473ff0d829377f5426b90e833e8456
eduardocaas <eduardofc.dev@gmail.com>
2023-06-29 23:34:46 -0300

    Fix details

--- src/main/java/com/efc/reactiveflashcards/api/controller/StudyController.java
+++ src/main/java/com/efc/reactiveflashcards/api/controller/StudyController.java
@@ -7,11 +7,10 @@ import com.efc.reactiveflashcards.domain.service.StudyService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -28,7 +27,7 @@ public class StudyController {
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(CREATED)
-    public Mono<QuestionResponse> start(final StudyRequest request) {
+    public Mono<QuestionResponse> start(@Valid @RequestBody final StudyRequest request) {
         return service.start(mapper.toDocument(request))
                 .doFirst(() -> log.info("==== try to create a study with follow request {}", request))
                 .map(document -> mapper.toResponse(document.getLastQuestionPending()));
