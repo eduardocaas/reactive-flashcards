1fb6766cc59b9999af8fb689dca69cf57e83f469
eduardocaas <eduardofc.dev@gmail.com>
2023-07-03 23:47:16 -0300

    Deck with pending Study treatment

--- src/main/java/com/efc/reactiveflashcards/domain/service/StudyService.java
+++ src/main/java/com/efc/reactiveflashcards/domain/service/StudyService.java
@@ -2,9 +2,12 @@ package com.efc.reactiveflashcards.domain.service;
 
 import com.efc.reactiveflashcards.domain.document.Card;
 import com.efc.reactiveflashcards.domain.document.StudyDocument;
+import com.efc.reactiveflashcards.domain.exception.DeckInStudyException;
+import com.efc.reactiveflashcards.domain.exception.NotFoundException;
 import com.efc.reactiveflashcards.domain.mapper.StudyDomainMapper;
 import com.efc.reactiveflashcards.domain.repository.StudyRepository;
 import com.efc.reactiveflashcards.domain.service.query.DeckQueryService;
+import com.efc.reactiveflashcards.domain.service.query.StudyQueryService;
 import com.efc.reactiveflashcards.domain.service.query.UserQueryService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +17,8 @@ import reactor.core.publisher.Mono;
 
 import java.util.Set;
 
+import static com.efc.reactiveflashcards.domain.exception.BaseErrorMessage.DECK_IN_STUDY;
+
 @Service
 @Slf4j
 @AllArgsConstructor
@@ -21,26 +26,37 @@ public class StudyService {
 
     private final UserQueryService userQueryService;
     private final DeckQueryService deckQueryService;
+    private final StudyQueryService studyQueryService;
     private final StudyRepository studyRepository;
     private final StudyDomainMapper studyDomainMapper;
 
     public Mono<StudyDocument> start(final StudyDocument document) {
-        return userQueryService.findById(document.userId())
+        return verifyStudy(document)
+                .then(userQueryService.findById(document.userId()))
                 .flatMap(user -> deckQueryService.findById(document.studyDeck().deckId()))
                 .flatMap(deck -> getCards(document, deck.cards()))
                 .map(study -> study.toBuilder()
-                            .question(studyDomainMapper.generateRandomQuestion(study.studyDeck().cards())).build())
+                        .question(studyDomainMapper.generateRandomQuestion(study.studyDeck().cards())).build())
                 .doFirst(() -> log.info("==== generating a first random question"))
                 .flatMap(studyRepository::save)
                 .doOnSuccess(study -> log.info("==== study saved {}", study));
     }
 
-    public Mono<StudyDocument> getCards(final StudyDocument document, final Set<Card> cards) {
+    private Mono<StudyDocument> getCards(final StudyDocument document, final Set<Card> cards) {
         return Flux.fromIterable(cards)
                 .doFirst(() -> log.info("==== copy cards to new study"))
                 .map(studyDomainMapper::toStudyCard)
                 .collectList()
                 .map(studyCards -> document.studyDeck().toBuilder().cards(Set.copyOf(studyCards)).build())
                 .map(studyDeck -> document.toBuilder().studyDeck(studyDeck).build());
+    }
+
+    private Mono<Void> verifyStudy(final StudyDocument document) {
+        return studyQueryService.findPendingStudyUserIdAndDeckId(document.userId(), document.studyDeck().deckId())
+                .flatMap(study -> Mono.defer(() ->
+                        Mono.error(new DeckInStudyException(DECK_IN_STUDY
+                                .params(document.userId(), document.studyDeck().deckId()).getMessage()))))
+                .onErrorResume(NotFoundException.class, e -> Mono.empty())
+                .then();
     }
 }
---
+++ src/main/java/com/efc/reactiveflashcards/domain/service/query/StudyQueryService.java
@@ -0,0 +1,29 @@
+package com.efc.reactiveflashcards.domain.service.query;
+
+import com.efc.reactiveflashcards.domain.document.StudyDocument;
+import com.efc.reactiveflashcards.domain.exception.NotFoundException;
+import com.efc.reactiveflashcards.domain.repository.StudyRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+import static com.efc.reactiveflashcards.domain.exception.BaseErrorMessage.STUDY_NOT_FOUND;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class StudyQueryService {
+
+    private final StudyRepository studyRepository;
+
+    public Mono<StudyDocument> findPendingStudyUserIdAndDeckId(final String userId, final String deckId) {
+        return studyRepository.findByUserIdAndCompleteTrueAndStudyDeck_DeckId(userId, deckId)
+                .doFirst(() -> log.info("==== try to get pending study with userId {} and deckId {}", userId, deckId))
+                .filter(Objects::nonNull)
+                .switchIfEmpty(Mono.defer(() ->
+                        Mono.error(new NotFoundException(STUDY_NOT_FOUND.params(userId, deckId).getMessage()))));
+    }
+}
