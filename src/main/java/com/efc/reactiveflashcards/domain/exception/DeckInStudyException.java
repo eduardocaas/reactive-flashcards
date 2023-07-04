2ed341a013486a2e1f90d363b99fe1b7fdaa8185
eduardocaas <eduardofc.dev@gmail.com>
2023-07-03 23:46:55 -0300

    Deck with pending Study exceptions

---
+++ src/main/java/com/efc/reactiveflashcards/domain/exception/DeckInStudyException.java
@@ -0,0 +1,8 @@
+package com.efc.reactiveflashcards.domain.exception;
+
+public class DeckInStudyException extends ReactiveFlashcardsException {
+
+    public DeckInStudyException(String message) {
+        super(message);
+    }
+}
